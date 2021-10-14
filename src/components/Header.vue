6739152d094daa7a18abbd6d3f949d80084b86ce
santiago-jv <santiagoolayojv@gmail.com>
2021-10-14 02:54:26 -0500

    fixing size of icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -94,7 +94,7 @@ export default {
         text-align: center;
         margin-bottom: 1.5rem;
     }
-    .container i{
+    .container i, .menu-container i{
         font-size: 1.5rem;
         color:var(--primary-color);
         cursor:pointer;
